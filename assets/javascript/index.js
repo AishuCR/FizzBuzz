9b3653094ad01ff68a7f277de203c52ad2f0d659
Aishu <crishwarya@gmail.com>
2019-06-27 15:11:38 -0700

    fixed endpoint for 0

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -19,6 +19,12 @@ function checkFizzBuzz() {
         return(output);
            
     }
+    else if (inputValue == 0)
+    {
+        output = document.getElementById("displayValue").innerHTML;
+        document.getElementById("displayValue").innerHTML = "0 is not a valid input";
+        return(output);
+    }
     else {
         output = document.getElementById("displayValue").innerHTML;
         document.getElementById("displayValue").innerHTML = "The number entered is not divisible by 3 or 5";
