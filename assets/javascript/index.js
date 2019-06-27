099db257a42f17f3410c0d0ad9ab77dae6298aad
Aishu <crishwarya@gmail.com>
2019-06-27 15:50:51 -0700

    edge case fixed

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -3,7 +3,14 @@ function checkFizzBuzz() {
 
     var inputValue = document.getElementById("myNumber").value;
 
-    if (((inputValue % 3) == 0) && ((inputValue % 5) == 0)) {
+    if (inputValue == 0)
+    {
+        output = document.getElementById("displayValue").innerHTML;
+        document.getElementById("displayValue").innerHTML = "0 is not a valid input";
+        return(output);
+    }
+
+    else if (((inputValue % 3) == 0) && ((inputValue % 5) == 0)) {
         output = document.getElementById("displayValue").innerHTML;
         document.getElementById("displayValue").innerHTML = "FizzBuzz";
         return(output);
@@ -19,12 +26,7 @@ function checkFizzBuzz() {
         return(output);
            
     }
-    else if (inputValue == 0)
-    {
-        output = document.getElementById("displayValue").innerHTML;
-        document.getElementById("displayValue").innerHTML = "0 is not a valid input";
-        return(output);
-    }
+    
     else {
         output = document.getElementById("displayValue").innerHTML;
         document.getElementById("displayValue").innerHTML = "The number entered is not divisible by 3 or 5";
